27a10354f6f9a69b735ea9b06dd4afc1f64ef50d
Jeremy Kay <jeremy@nobledesktop.com>
2012-07-16 12:53:15 -0400

    install rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NobleRails::Application.config.secret_token = 'd25cf5bda5cef3b30a8cef0fc4f42c146942a23b75821075178df726bde51c567bdeb696277381575c59e61ab4b4324ae9d1af051266412592e6fe7518c94a88'
