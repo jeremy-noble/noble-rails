55712ab05633dc20858e1d8b6ca50d30a513672f
Jeremy Kay <jeremy@nobledesktop.com>
2012-07-19 13:58:03 -0400

    getting form together

--- app/models/seminar_time.rb
+++ app/models/seminar_time.rb
@@ -1,5 +1,5 @@
 class SeminarTime < ActiveRecord::Base
   attr_accessible :end_date, :seminar_id, :start_date
-  belongs_to :seminar, :foreign_key => 'seminar_id'
+  belongs_to :seminar
   default_scope :order => 'start_date'
 end
