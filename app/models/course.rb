2652cbea70a7e7750ac92b2d0314cdc9d9c84b34
jeremykay <jeremykaynyc@gmail.com>
2012-08-08 19:32:43 -0400

    edited event and course model associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ActiveRecord::Base
   attr_accessible :name
 
   has_many :events
+  has_many :sessions, :through => :events
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :course_id
 
-  belongs_to :course
+  has_one :course
   has_many :sessions
 
 
