a9a108e81ed48f8816a3f0136d6fee777be240b9
Jeremy Kay <jeremy@nobledesktop.com>
2012-08-13 14:50:02 -0400

    custom registration validations

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,4 +6,28 @@ class Registration < ActiveRecord::Base
 
   validates :event_id, presence: true
   validates :user_id, presence: true
+  validate :event_exists
+  validate :user_exists
+
+  after_create :new_registration_notification
+
+  def event_exists
+    if !Event.exists?(event_id)
+      errors.add(:Event, 'does not exist.')
+    end
+  end
+
+  def user_exists
+    if !User.exists?(user_id)
+      errors.add(:User, 'does not exist.')
+    end
+  end
+
+
+  private
+
+    def new_registration_notification
+      RegistrationMailer.new_registration(self).deliver
+    end
+
 end
