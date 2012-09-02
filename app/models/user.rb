23e6ce71609a8386d99d578e6e55dc9b5331a2c0
jeremykay <jeremykaynyc@gmail.com>
2012-09-02 18:04:23 -0400

    getting enews signups together

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
   validates :first_name, length: { maximum: 80 }
   validates :last_name, length: { maximum: 80 } 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
